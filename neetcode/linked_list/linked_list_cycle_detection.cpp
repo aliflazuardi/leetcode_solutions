544c53b6c4f51fb711e87f12e3dfb706c3a280cd
aliflazuardi <aliflazuardi250999@gmail.com>
2024-06-17 17:34:45 +0700

    add neetcode linked list cycle detection solution using fast and slow pointers in cpp

--- neetcode/linked_list/linked_list_cycle_detection.cpp
+++ neetcode/linked_list/linked_list_cycle_detection.cpp
@@ -26,3 +26,24 @@ public:
         return false;
     }
 }
+
+// solution using fast and slow pointers
+/* class Solution { */
+/* public: */
+/*     bool hasCycle(ListNode* head) { */
+/*         ListNode* fast = head; */
+/*         ListNode* slow = head; */
+/**/
+/*         while (fast != NULL && fast->next != NULL) { */
+/*             fast = fast->next->next; */
+/*             slow = slow->next; */
+/**/
+/*             if (fast == slow) { */
+/*                 return true; */
+/*             } */
+/*         } */
+/**/
+/*         return false; */
+/*     } */
+/* }; */
+

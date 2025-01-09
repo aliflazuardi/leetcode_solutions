ecd480a0af334a6d30e498389b80968f4bf1ebdb
aliflazuardi <aliflazuardi250999@gmail.com>
2025-01-09 21:47:48 +0700

    optimize leetcode solution for problem no 1 in golang

--- leetcode/1_Two_Sum/Solution.go
+++ leetcode/1_Two_Sum/Solution.go
@@ -1,18 +1,15 @@
 func twoSum(nums []int, target int) []int {
-    m := make(map[int]int)
-    var result[]int
-    var found bool
-    
-    for i, num := range nums {
-        diff := target - num
-        if _, ok := m[diff]; ok {
-            if !found {
-                result = append(result, m[diff])
-                result = append(result, i)
-                found = true
-            }
-        }
-        m[num] = i
-    }
-    return result
+	m := make(map[int]int)
+	var result []int
+
+	for i, num := range nums {
+		diff := target - num
+		if _, ok := m[diff]; ok {
+			result = append(result, m[diff])
+			result = append(result, i)
+			break
+		}
+		m[num] = i
+	}
+	return result
 }

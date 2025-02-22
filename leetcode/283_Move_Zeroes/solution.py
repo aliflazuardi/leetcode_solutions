a3baac2a5edec0eec65afa398d385aec3e27c160
aliflazuardi <aliflazuardi250999@gmail.com>
2025-02-22 11:03:18 +0700

    add leetcode problem no 283 improved implementaion using python3 (5ms, beats 51.36%)

--- leetcode/283_Move_Zeroes/solution.py
+++ leetcode/283_Move_Zeroes/solution.py
@@ -16,3 +16,19 @@ class Solution:
             nums[p] = temp
 
             z += 1
+
+
+class Solution2:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        nz = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[nz] = nums[i]
+                nz += 1
+
+        for i in range(nz, len(nums)):
+            nums[i] = 0

9a189f4f0877d461e4f4522ea4b373e0dab9fd64
aliflazuardi <aliflazuardi250999@gmail.com>
2025-02-23 13:55:10 +0700

    add leetcode problem no 2095 solution using python3 (68ms, beats 32.28%)

---
+++ leetcode/2095_Delete_the_Middle_Node_of_a_Linked_List/solution.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head.next is None:
+            return None
+        fp = head
+        sp = head
+
+        while fp is not None:
+            fp = fp.next
+            if fp is None or fp.next is None:
+                break
+            fp = fp.next
+            if fp.next is not None:
+                sp = sp.next
+
+        np = sp.next
+        if np is not None:
+            sp.next = np.next
+
+        return head

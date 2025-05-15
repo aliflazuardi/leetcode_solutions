e5a6516fb32f49f415fbf3ec6fd855b805b7c8a8
aliflazuardi <aliflazuardi250999@gmail.com>
2025-05-15 22:21:43 +0700

    lc 3467 python3

---
+++ leetcode/3467_Transform_Array_by_Parity/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def transformArray(self, nums: List[int]) -> List[int]:
+        odd_count = 0
+        even_count = 0
+
+        for num in nums:
+            if num % 2 == 0:
+                even_count += 1
+            else:
+                odd_count += 1
+
+        return ([0] * even_count) + ([1] * odd_count)

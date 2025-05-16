95cfbb6a3142498e6269b1ba05007f75f462a6af
aliflazuardi <aliflazuardi250999@gmail.com>
2025-05-16 21:56:46 +0700

    lc 1108 python3

---
+++ leetcode/1108_Defanging_an_IP_Address/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        ans = ""
+
+        for c in address:
+            if c == ".":
+                ans += "[.]"
+            else:
+                ans += c
+
+        return ans

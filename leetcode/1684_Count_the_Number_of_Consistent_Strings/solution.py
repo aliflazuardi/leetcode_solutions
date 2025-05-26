d695fa52635e9125ffdd51b3506c37e5030585ec
aliflazuardi <aliflazuardi250999@gmail.com>
2025-05-26 23:05:32 +0700

    lc 1584 python3

---
+++ leetcode/1684_Count_the_Number_of_Consistent_Strings/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        ans = len(words)
+
+        for word in words:
+            for c in word:
+                if c not in allowed:
+                    ans -= 1
+                    break
+
+        return ans

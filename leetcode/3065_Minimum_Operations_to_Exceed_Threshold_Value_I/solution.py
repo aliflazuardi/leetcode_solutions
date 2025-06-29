b7577917d4f5fdd4dcfbf9ace8f76ac8c834dc88
aliflazuardi <aliflazuardi250999@gmail.com>
2025-06-29 20:14:44 +0700

    lc 3065 python3

---
+++ leetcode/3065_Minimum_Operations_to_Exceed_Threshold_Value_I/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        op = 0
+
+        for num in nums:
+            if num < k:
+                op += 1
+
+        return op
